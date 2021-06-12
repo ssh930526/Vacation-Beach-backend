21eed6c7d0fe865b2560593e527d7b604c26f0f6
Sihui Shen <sihui@Sihuis-MacBook-Pro.local>
2021-06-12 15:32:18 -0500

    wipupdate final changes

--- models/beach.js
+++ models/beach.js
@@ -5,17 +5,9 @@ const Schema = mongoose.Schema;
 const beachSchema = new Schema ({
     beachName: String,
     beachLocated: String,
-    sandsColor: String,
-    rating: String},{ timestamps: true 
-
-
-// rating: {
-//             type: Number,
-//             min: 1,
-//             max: 10,
-//             default: 10
-//         }
-//         },{ timestamps: true }
+    sandColor: String,
+    rating: String },
+    { timestamps: true 
 
 });
 
